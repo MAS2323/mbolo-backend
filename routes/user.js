4c4625cf7bbcacec50047fd0bb88e2150280e9b8
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2025-01-29 15:23:53 +0800

    arreglando las rutas de usr

--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ import userController from "../controllers/userControllers.js";
 router.post("/register", userController.registerUser);
 router.post("/login", userController.loginUser);
 router.get("/users/:userId", userController.getUsersExceptLoggedInUser);
-router.put("/users/:id", upload.single("image"), userController.updateUser);
+router.put("/user/:id", upload.single("image"), userController.updateUser);
 router.get("/users", userController.getAllUsers); // Ruta protegida
 router.get("/user/:id", userController.getUserById); // Ruta protegida
 router.delete("/user/:id", userController.deleteUser); // Ruta protegida
