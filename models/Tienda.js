338c8d85ad5009bcfc8e80b976370ea504215ee8
Ernesto Vicencio Mas <masoneweernesto@gmail.com>
2025-03-21 14:29:22 +0800

    primer commit

--- models/Tienda.js
+++ models/Tienda.js
@@ -1,57 +1,64 @@
-const UserSchema = new mongoose.Schema(
+import mongoose from "mongoose";
+
+// Definir el esquema de la tienda
+const tiendaSchema = new mongoose.Schema(
   {
-    userName: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    image: { type: String },
-    location: { type: String, required: true },
-    mobile: { type: String, required: true },
-    userType: { type: String },
-    friendRequests: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    friends: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    sentFriendRequests: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logo_url: {
+      type: String,
+      required: true,
+    },
+    banner_url: {
+      type: String,
+      required: true,
+    },
+    contact_email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+      match: [
+        /\S+@\S+\.\S+/,
+        "Por favor ingresa un correo electrónico válido.",
+      ],
+    },
+    phone_number: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", // Relaciona con el modelo de usuario
+      required: true,
+    },
     products: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
+        ref: "Product", // Relaciona con el modelo de producto (opcional, si lo usas)
+        required: true,
       },
     ],
-    order: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Order",
-      },
-    ],
-    carts: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Cart",
-      },
-    ],
-    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Subcategory" }],
-
-    // Nuevo campo para referenciar la tienda del usuario
-    tienda: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Tienda",
-    },
   },
   {
-    timestamps: true,
+    timestamps: true, // Agrega los campos createdAt y updatedAt
   }
 );
+
+// Crear el modelo de Tienda
+const Tienda = mongoose.model("Tienda", tiendaSchema);
+
+export default Tienda;
